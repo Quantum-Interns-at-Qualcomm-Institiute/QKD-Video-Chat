55f21dc7301428a2f9f516f6b6a6ebe97150422b
Aaron <aaron.awesome.kirk@gmail.com>
2023-09-13 15:32:28 -0700

    start page

--- frontend/src/screens/Start.js
+++ frontend/src/screens/Start.js
@@ -2,7 +2,8 @@ import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import Header from "../components/Header";
 import {isValidCode} from "../util/Auth";
-import { Snackbar } from "@material-ui/core";
+import { IconButton, Snackbar } from "@material-ui/core";
+import CloseIcon from '@mui/icons-material/Close';
 
 import "../css/Start.css"
 
@@ -21,15 +22,24 @@ export default function Start() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const response = await isValidCode(code);
-        if(response.ok) navigate("/session")
-        // else show error
+        console.log(response)
+        if(response.ok) navigate(`/session/${code}`)
+        else if (response.message) setError({
+            open: true,
+            message: response.message})
+        else setError({...error, open: true})
+    }
+
+    const handleClose = (e, reason) =>{
+        if(reason === "clickaway") return;
+        setError({...error, open: false})
     }
 
     return (
         <>
             <Header />
-            <div class="start-content">
-                <form class="codeForm" onSubmit={handleSubmit}>
+            <div className="start-content">
+                <form className="codeForm" onSubmit={handleSubmit}>
                     <input type="text" placeholder="Code" name="code" id="code" onChange={handleCodeChange}/>
                     <button type="submit">Connect</button>
                 </form>
@@ -38,10 +48,13 @@ export default function Start() {
             <Snackbar
                 open={error.open}
                 autoHideDuration={6000}
-                onClose={(e,reason) => {
-                    if(reason === "clickaway") return;
-                    setError({...state})
-                }}
+                message={error.message}
+                onClose={handleClose}
+                action={
+                    <IconButton size="small" onClick={handleClose}>
+                        <CloseIcon fontSize="small" />
+                    </IconButton>
+                }
             />
         </>
     )
--- frontend/src/util/Auth.js
+++ frontend/src/util/Auth.js
@@ -1,8 +1,16 @@
 async function isValidCode(code) {
 
     let isDigit = /^\d+$/.test(code);
-    if (isDigit && code != "" && code.length == 8) return {ok: true}
-    return {ok: false};
+    if (code === "") return {
+        ok: false,
+        message: "Please enter a valid code."}
+    else if (!isDigit) return {
+        ok: false,
+        message: "Code may only contain numeric characters."}
+    else if (code.length !== 8) return {
+        ok: false,
+        message: "Code must be strictly 8 characters."}
+    else return {ok: true, message: "Valid code."};
 }
 
 export {isValidCode};
