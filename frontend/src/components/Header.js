6f0b4d69816bd6120fdb4e0640335ac5a7e1e88a
Aaron <aaron.awesome.kirk@gmail.com>
2023-09-13 15:32:20 -0700

    header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,11 +6,11 @@ import "../css/Header.css";
 export default function Header(props) {
 
     return(
-        <div class="header">
+        <div className="header">
             <img src={Logo} alt="UCSD Logo" id="logo" />
             <h1>QKD Video Chat</h1>
             {props.showWaveforms ? 
-            <div class="waveforms">
+            <div className="waveforms">
                 {/*<img src={Waveforms} alt="Waveforms" id="waveforms" />*/}
                 <p>Waveforms</p>
             </div> : null}
